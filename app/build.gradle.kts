37dbc6e54d8e29d020f378c2a807e1c3fa5db94b
Ibolt <imaangill@outlook.com>
2023-10-11 13:49:29 -0400

    Setup compose for desktop in project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,18 +4,25 @@
  * This generated file contains a sample Kotlin application project to get you started.
  * For more details on building Java & JVM projects, please refer to https://docs.gradle.org/8.3/userguide/building_java_projects.html in the Gradle documentation.
  */
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
 
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
+    // Compose
+    id("org.jetbrains.compose") version "1.5.3"
+
+//    // Apply the application plugin to add support for building a CLI application in Java.
+//    application
 }
 
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
 }
 
 dependencies {
@@ -28,7 +35,7 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // This dependency is used by the application.
-    implementation("com.google.guava:guava:32.1.1-jre")
+    implementation(compose.desktop.currentOs)
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -38,12 +45,24 @@ java {
     }
 }
 
-application {
-    // Define the main class for the application.
-    mainClass.set("cs.AppKt")
-}
+//application {
+//    // Define the main class for the application.
+//    mainClass.set("cs.AppKt")
+//}
 
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+}
+
+compose.desktop {
+    application {
+        mainClass = "cs.AppKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "cs"
+            packageVersion = "1.0.0"
+        }
+    }
 }
--- app/src/main/kotlin/cs/App.kt
+++ app/src/main/kotlin/cs/App.kt
@@ -3,6 +3,11 @@
  */
 package cs
 
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
 class App {
     val greeting: String
         get() {
@@ -10,6 +15,16 @@ class App {
         }
 }
 
-fun main() {
-    println(App().greeting)
+fun main() = application {
+    Window(
+        title = "hi",
+        onCloseRequest = ::exitApplication
+    ) {
+        Greeting("Compose")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text("Hello $name!")
 }
